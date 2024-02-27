dbba1559f4a4f08aecc3206a597fb93e833d0f25
NDSTechBlog <161292560+NDSTechBlog@users.noreply.github.com>
2024-02-27 10:00:38 +0900

    Update site.config.js
    change logo name

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "NDS Tech Blog",
-    image: "/nds_logo_fixed.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/nds_logo.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "NDS",
     bio: "NDS는 농심 그룹의 정보시스템을 개발∙운영해온 전산실을 모태로 설립하였으며, 다양한 프로젝트 경험과 축적된 기술 및 노하우를 바탕으로 최적의 IT서비스를 제공하는 기업입니다.",
     email: "nds37371@gmail.com",
