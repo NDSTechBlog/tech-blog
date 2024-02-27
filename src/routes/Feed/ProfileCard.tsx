6ef8bfff070b2011669a6b6e5672feea2e5eab05
NDSTechBlog <nds37371@gmail.com>
2024-02-27 16:13:55 +0900

    [FIX] profile card style 오류 수정(객체형)

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -14,7 +14,7 @@ const ProfileCard: React.FC<Props> = () => {
       </div>
       <div className="content">
         <div className="top">
-          <Image src={CONFIG.profile.image} fill alt="" style={width: "140px", height: "auto"} />
+          <Image src={CONFIG.profile.image} fill alt="" style={{width: "140px", height: "auto"}} />
         </div>
         <div className="mid">
           <div className=" name">{CONFIG.profile.name}</div>
