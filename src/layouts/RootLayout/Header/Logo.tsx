361579f301e0f1259932a070427dd0215ac400e8
NDSTechBlog <nds37371@gmail.com>
2024-02-28 15:08:29 +0900

    [FIX] 타이틀 로고 이미지 변경

--- src/layouts/RootLayout/Header/Logo.tsx
+++ src/layouts/RootLayout/Header/Logo.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link"
+import Image from "next/image"
 import { CONFIG } from "site.config"
 import styled from "@emotion/styled"
+import useScheme from "src/hooks/useScheme"
 
 const Logo = () => {
+  const [scheme] = useScheme();
+
   return (
     <StyledWrapper href="/" aria-label={CONFIG.blog.title}>
-      {CONFIG.blog.title}
+      {/* {CONFIG.blog.title} */}
+      <Image src={scheme === "light" ? CONFIG.blog.titleImage : CONFIG.blog.titleImage} fill alt="" />
     </StyledWrapper>
   )
 }
